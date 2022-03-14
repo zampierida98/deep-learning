968f82235fb00cd424794eaeed88abd4b7002762
dallachiaram <dallachiaram@gmail.com>
2022-03-14 13:21:34 +0100

    realizzazione corpo principale

--- test_MPII.py
+++ test_MPII.py
@@ -4,20 +4,104 @@ modelli sul dataset MPII. L'insieme delle funzioni di supporto come la metrica i
 in un file ausiliario così da poterla usare in un momento successivo
 '''
 # IMPORT
+import pickle
 import os
-import sys
-# VARIABILI E COSTANTI
+import utils
+import numpy as np
+import warnings
+from itertools import groupby
+warnings.simplefilter("ignore", FutureWarning)
 
+# COSTANTI
 # dataset path
 #DS_PATH = './MPII/images'
 DS_PATH = './MPII/sub_dataset'
-
 EFFICIENTPOSE_PATH = './EfficientPose-master/'
 EFFICIENTPOSE_MAIN = 'track.py'
 MODEL = ['RT', 'RT_Lite','I','I_Lite', 'II','II_Lite', 'III', 'IV'][5]
+JOINT_ID = {0:"right_ankle", 1:"right_knee", 2: "right_hip", 3: "left_hip", 4: "left_knee", 5: "left_ankle", 6: "pelvis", 7:"thorax", 8:"upper_neck", 9:"head_top", 10:"right_wrist", 11:"right_elbow", 12:"right_shoulder", 13:"left_shoulder", 14:"left_elbow", 15: "left_wrist"}
+# VARIABILI
+annotations = None
+create_csv_model_infer = False
 # FUNZIONI
+def get_rows_from_annotations(annotations, abs_ds_path):
+    res = {}
+    for name in os.listdir(abs_ds_path):
+        if name.split(".")[1] == "csv":
+            continue
+        for row in annotations:
+            if name == row['image']['name']:
+                try:
+                    parts = {}
+                    _ = row['annopoints']
+                    for d in row['annopoints']['point']: # 16 parti del corpo
+                        parts[JOINT_ID[d['id']]] = (d['x'], d['y'])
+                    res[name] = parts
+                except:
+                    break
+    return res
+
+def get_rows_from_csv(list_of_file, abs_ds_path):
+    res = {}
+    for file in list_of_file:
+        with open(f'{abs_ds_path}\\{file.split(".")[0]}_coordinates.csv', 'r') as fin:
+            header = fin.readline().strip()
+            header = header.replace("_x", ":x")
+            header = header.replace("_y", ":y")
+            fin.readline() # riga vuota
+            value = fin.readline().strip()
+            body_parts = list(zip(header.split(","), value.split(",")))
+            body_parts.sort(key=lambda x: x[0])
+            parts = {}
+            for p in [list(i) for j, i in groupby(body_parts, lambda x: x[0].split(':')[0])]:
+                # p = [[parte_corpo_x, val_x], [parte_corpo_y, val_y]]
+                parts[p[0][0].split(":")[0]] = (p[0][1], p[1][1])
+                res[file] = parts
+    return res
 # MAIN
 if __name__ == "__main__":
-    for file in os.listdir(DS_PATH):
-        abs_file_path = os.path.abspath(DS_PATH) + "/"
-        os.system(f'python {EFFICIENTPOSE_PATH+EFFICIENTPOSE_MAIN}.py -m={MODEL} -p={abs_file_path+file} -s')
+    with open('annotations.pickle', 'rb') as fin:
+        annotations = pickle.load(fin)
+    
+    # path assoluto della dir DS_PATH
+    abs_ds_path = os.path.abspath(DS_PATH)    
+
+    # vengono creati i csv se necessario
+    if create_csv_model_infer:
+        list_of_file = []
+        for file in os.listdir(abs_ds_path):
+            if not file.endswith(".csv"):
+                list_of_file.append(f'{abs_ds_path}\\{file}')
+                
+        os.chdir(EFFICIENTPOSE_PATH)
+
+        for file in list_of_file:
+            os.system(f'python {EFFICIENTPOSE_MAIN} --model={MODEL} --path="{file}" --store')
+    
+    # descrizione della struttura link: http://human-pose.mpi-inf.mpg.de/#download
+    # Non per tutte le img abbiamo le predizioni.
+    # TODO: è da investigare su questo fatto
+
+    ground_truth = get_rows_from_annotations(annotations, abs_ds_path)
+    # alcune img non hanno il campo annopoints per cui tali img non verranno recuperate da inference
+    
+    '''
+    Parte poi da togliere ma la lascio adesso
+    '''
+    '''
+    ################################################################################
+    '''
+    ground_truth_keys = []
+    for file in os.listdir(abs_ds_path):
+        if not file.endswith(".csv"): # perchè vengono dalle chiavi in gt
+            ground_truth_keys.append(file)
+    '''
+    ################################################################################
+    '''    
+    inference = {}#get_rows_from_csv(ground_truth_keys, abs_ds_path) #ground_truth.keys()
+
+    print(ground_truth)
+    print("######################")
+    print(inference)
+        
+
---
+++ utils.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pickle
+from scipy.io import loadmat, matlab
+
+# FUNZIONI
+def load_mat(filename):
+    """
+    This function should be called instead of direct scipy.io.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    """
+
+    def _check_vars(d):
+        """
+        Checks if entries in dictionary are mat-objects. If yes
+        todict is called to change them to nested dictionaries
+        """
+        for key in d:
+            if isinstance(d[key], matlab.mio5_params.mat_struct):
+                d[key] = _todict(d[key])
+            elif isinstance(d[key], np.ndarray):
+                d[key] = _toarray(d[key])
+        return d
+
+    def _todict(matobj):
+        """
+        A recursive function which constructs from matobjects nested dictionaries
+        """
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, matlab.mio5_params.mat_struct):
+                d[strg] = _todict(elem)
+            elif isinstance(elem, np.ndarray):
+                d[strg] = _toarray(elem)
+            else:
+                d[strg] = elem
+        return d
+
+    def _toarray(ndarray):
+        """
+        A recursive function which constructs ndarray from cellarrays
+        (which are loaded as numpy ndarrays), recursing into the elements
+        if they contain matobjects.
+        """
+        if ndarray.dtype != 'float64':
+            elem_list = []
+            for sub_elem in ndarray:
+                if isinstance(sub_elem, matlab.mio5_params.mat_struct):
+                    elem_list.append(_todict(sub_elem))
+                elif isinstance(sub_elem, np.ndarray):
+                    elem_list.append(_toarray(sub_elem))
+                else:
+                    elem_list.append(sub_elem)
+            return np.array(elem_list)
+        else:
+            return ndarray
+
+    data = loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_vars(data)
+
+def pckh():
+    pass
+
+if __name__ == "__main__":
+    mat = load_mat('annotations.mat')
+    print(mat['RELEASE']['annolist'][100:103])
+
+    with open("annotations.pickle", 'wb') as fout:  # Overwrites any existing file.
+        pickle.dump(mat['RELEASE']['annolist'], fout, pickle.HIGHEST_PROTOCOL)
