e3c4fe8e1b29c295234b85771192e3a450f446cd
zampierida98 <zampierida98@gmail.com>
2022-07-01 16:11:55 +0200

    inferenza sulla persona ritagliata dai frame del video

--- DETECTOR/SCRIPT_YOLO.py
+++ DETECTOR/SCRIPT_YOLO.py
@@ -16,20 +16,18 @@ warnings.simplefilter("ignore", FutureWarning)
 DS_PATH = './DATASET/'
 THRESHOLD = 0.5
 
-"""
+
 RES_PATH = './results/'
-EFFICIENTPOSE_PATH = '../EfficientPose-master/'
+EFFICIENTPOSE_PATH = '../EfficientPose-master'
 EFFICIENTPOSE_MAIN = 'track.py'
 MODELS = ['RT','I','II','III','IV']
 FRAMEWORK = 'pytorch'
+OUTPUT = '--visualize'
 MODEL = MODELS[4]
-
-# VARIABILI
-# True lavora su un solo modello (MODEL), False crea un plot che compara i diversi modelli
-ANALYZE_ONE_MODEL = False
+ANALYZE_ONE_MODEL = False  # True lavora su un solo modello (MODEL), False su tutti i modelli
 
 # FUNZIONI
-def create_csv_model_inference(abs_ds_path, abs_res_path, model):
+def model_inference(abs_ds_path, abs_res_path, model):
     # lista con i nomi delle sotto-cartelle
     dirs = [ f for f in os.listdir(abs_ds_path) if not os.path.isfile(os.path.join(abs_ds_path,f)) ]
 
@@ -48,28 +46,28 @@ def create_csv_model_inference(abs_ds_path, abs_res_path, model):
         except:
             pass
 
-        # per ogni file viene creato il csv
         for file in list_of_file:
-            os.system(f'python3.7 {EFFICIENTPOSE_MAIN} --model={model} --path="{file}" --store --framework={FRAMEWORK}')
+            os.system(f'python3.7 {EFFICIENTPOSE_MAIN} --path="{file}" --framework={FRAMEWORK} --model={model} {OUTPUT}')
 
-        # sposto i csv nella cartella dei risultati
+        # sposto i risultati in RES_PATH
         for file in os.listdir(dir_path):
-            if os.path.isfile(os.path.join(dir_path,file)) and file.endswith(".csv"):
+            if os.path.isfile(os.path.join(dir_path,file)) and (file.endswith(".csv") or "_tracked" in file):
                 os.replace(f'{dir_path}/{file}', f'{abs_res_path}/{model}/{file}')
 
-def main_LSP():
+def infer():
     # path assoluto della cartella DS_PATH
     abs_ds_path = os.path.abspath(DS_PATH)
     abs_res_path = os.path.abspath(RES_PATH)
     
-    # vengono creati i csv
+    # vengono prodotti i risultati
     if ANALYZE_ONE_MODEL:
-        create_csv_model_inference(abs_ds_path, abs_res_path, MODEL)
+        model_inference(abs_ds_path, abs_res_path, MODEL)
 
     else:
         for m in MODELS:
-            create_csv_model_inference(abs_ds_path, abs_res_path, m)
-"""
+            model_inference(abs_ds_path, abs_res_path, m)
+
+
 def detect():
     # Model
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
@@ -103,6 +101,8 @@ def crop(people):
 
 
 if __name__ == "__main__":
+    infer()
+    """
     detections = detect()
     
     people = {}
@@ -115,3 +115,4 @@ if __name__ == "__main__":
         people[img] = bbox.loc[bbox['name'] == 'person']
     
     crop(people)
+    """
